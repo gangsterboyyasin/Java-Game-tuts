517886c56ab5248fc4b5f98d6131a7c515ee4dd6
yasin <yasinsevik@live.nl>
2013-12-20 14:14:55 +0100

    Een paar bugs verwijdert
    hierom raad ik je aan de code in eclipse te bekijken

--- src/nl/retronet/tuts/Game.java
+++ src/nl/retronet/tuts/Game.java
@@ -1,17 +1,20 @@
-package nl.retronet.tuts
+package nl.retronet.tuts;
 
 import javax.swing.*;
+
 import java.awt.*;
 
-public class Main extends Canvas implements Runnable{
+//een waarschuwing supressor negeert onnodige waarschuwingen die java geeft
+@SuppressWarnings("serial")
+public class Game extends Canvas implements Runnable{
 	/*
 	*Wees zeker dat je de Eclipse IDE gebruikt voor dieper uitkijk
 	*/
 	
 	//grijpt de venster eigenschappen en ze worden doorgegeven in de createJFrame method
-	public static String title;
-	public static final int WIDTH;
-	public static final int HEIGHT;
+	public String title;
+	public int width;
+	public int height;
 	
 	//een boolean om te beslissen of je game loopt of niet
 	public boolean isRunning = false;
@@ -20,53 +23,66 @@ public class Main extends Canvas implements Runnable{
 	private Thread thread;
 	
 	//lege constructor
-	public Main(){
+	public Game(){
+		//het roept de createJFrame method
+		createJFrame(800, 600, "Frame");
+		
 		//defineert de thread object om een Thread te worden (ps de this argument in thread houd de instance van thread dus waar je Runnable erbij haalt)
 		thread = new Thread(this);
 		thread.start();
 	}
 	
 	//de start method wordt geroepen als de venster is gecreeÃ©rd
-	public static void start(){
+	public void start(){
 		if(isRunning)
-			break;
+			return;
 			
 		isRunning = true;
+		System.out.println("Game loopt");
 	}
 	
 	//een method om de game stoppen(allen als het een applet is)
-	public static void stop(){
+	public void stop(){
 		if(!isRunning) 
-			break;
+			return;
 			
 			isRunning = false;
 	}
 	
 	// een method dat elke milliseconden geroept wordt
 	public void run(){
-	
 		//de game loop dus zolang isRunning true is doe wat er hier staat
 		while(isRunning){
-			
-		
+			//roept de render en update methods die we gaan gebruiken voor de Game(momenteel leeg)
+			update();
+			render();
 		}
 	
 	
 	}
 	
+	public void render() {
+		
+	}
+
+	public void update() {
+		
+	}
+
 	//main method
 	public static void main(String[] arg0){
-		System.out.println("HelloWorld");
+		System.out.println("Game Booted!");
 		
-		//het roept de createJFrame method
-		createJFrame(800, 600, "Frame");
+		//roept de constructor van Game.java
+		new Game();
 	}
 	
-	public static void creatJFrame(int width, int height, String title){
+	public void createJFrame(int width, int height, String title){
 		//geef de argumenten in de brackets van createJFrame aan de variablelen die we boven hebben gemaakt
-		this.WIDTH = width;
-		this.HEIGHT = height;
+		this.width = width;
+		this.height = height;
 		this.title = title;
+		System.out.println("eigenschappen gekregen");
 		
 		//je maakt en defineert de JFrame dat is java's hulpclass voor ven=sters
 		JFrame frame = new JFrame();
@@ -75,11 +91,14 @@ public class Main extends Canvas implements Runnable{
 		frame.setResizable(false);
 		frame.pack();
 		frame.setTitle(title);
-		frame.setSize(WIDTH, HEIGHT);
+		frame.setSize(width, height);
 		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		System.out.println("Venster Gecreeérd");
 		
 		//roept de start method
 		start();
+	}
 }
